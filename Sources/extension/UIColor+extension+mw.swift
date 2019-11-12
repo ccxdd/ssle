5d6e0fec1b6034a9e9de3c276a63e0c37f729902
ccxdd <e_xiaodong@hotmail.com>
2019-11-12 15:47:25 +0800

    UIColor extension

--- Sources/extension/UIColor+extension+mw.swift
+++ Sources/extension/UIColor+extension+mw.swift
@@ -34,4 +34,72 @@ public extension UIColor {
         return withAlphaComponent(a)
     }
 }
+
+public extension UIColor {
+    enum CompatibleiOS13: Int {
+        case text1, text2, text3, text4
+        case bg1, bg2, bg3
+        case group1, group2, group3
+        case gray2, gray3, gray4, gray5, gray6
+        case placeholder
+        case link
+        case separator, opaqueSeparator
+        case fill1, fill2, fill3, fill4
+    }
+    
+    static func compatible(c: CompatibleiOS13, light: UIColor) -> UIColor {
+        if #available(iOS 13.0, *) {
+            switch c {
+            case .bg1:
+                return .systemBackground
+            case .bg2:
+                return .secondarySystemBackground
+            case .bg3:
+                return .tertiarySystemBackground
+            case .group1:
+                return .systemGroupedBackground
+            case .group2:
+                return .secondarySystemGroupedBackground
+            case .group3:
+                return .tertiarySystemGroupedBackground
+            case .text1:
+                return .label
+            case .text2:
+                return .secondaryLabel
+            case .text3:
+                return .tertiaryLabel
+            case .text4:
+                return .quaternaryLabel
+            case .gray2:
+                return .systemGray2
+            case .gray3:
+                return .systemGray3
+            case .gray4:
+                return .systemGray4
+            case .gray5:
+                return .systemGray5
+            case .gray6:
+                return .systemGray6
+            case .placeholder:
+                return .placeholderText
+            case .link:
+                return .link
+            case .separator:
+                return .separator
+            case .opaqueSeparator:
+                return .opaqueSeparator
+            case .fill1:
+                return .systemFill
+            case .fill2:
+                return .secondarySystemFill
+            case .fill3:
+                return .tertiarySystemFill
+            case .fill4:
+                return .quaternarySystemFill
+            }
+        } else {
+            return light
+        }
+    }
+}
 #endif
